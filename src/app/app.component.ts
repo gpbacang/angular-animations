e2b69a71faa5099eb77de76ab39baf1323cb1ae3
Geraldine <geraldine@g-angle.co.jp>
2017-02-23 14:46:17 +0800

    Add advanced animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import {
   Component,
   trigger,
   state,
-  style
+  style,
+  transition,
+  animate
 } from '@angular/core';
 
 @Component({
@@ -18,13 +20,44 @@ import {
         'background-color': 'blue',
         transform: 'translateX(100px)'
 
-      }))
+      })),
+      transition('normal <=> highlighted', animate(300))
+      // transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform : 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
   onAdd(item) {
     this.list.push(item);
